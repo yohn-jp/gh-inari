7753d40882f8dc2f52c3ccf290701cb0050fa397
yohnark <213253858+oy-zenprax@users.noreply.github.com>
2026-09-06 17:05:22 +0900

    chore: rebuild dist after rebase onto main

--- dist/index.js
+++ dist/index.js
@@ -22,6 +22,7 @@ export * from "./change-trusted-executor.js";
 export * from "./semantic-pr-projection.js";
 export * from "./semantic-issue-projection.js";
 export * from "./semantic-branch-projection.js";
+export * from "./semantic-pr-executor.js";
 export * from "./artifact-contract-governance.js";
 export { discoverTemplates, discoverTemplatesSync, discoverTemplatesFromPaths, classifyTemplatePath, isTemplateContainerPath, isTemplatePathInNativeDirectory, selectTemplate, selectIssueTemplate, selectPullRequestTemplate, TemplateDiscoveryError, TemplateFilesystemError, TemplateNotFoundError, TemplateSelectionAmbiguousError, TemplateNameConflictError, InvalidTemplateSelectorError, } from "./template-discovery.js";
 let invokedPath;
